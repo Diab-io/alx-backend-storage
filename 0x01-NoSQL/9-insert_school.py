c71e19d51d83971312781a98765ad65746faa249
Odimayo David <odimayodavid7@gmail.com>
2023-04-13 22:34:57 +0100

    Added function to insert a new doc

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" inserts a new document in the collection based off kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a document to the provided collection """
+    doc_data = mongo_collection.insertOne(kwargs)
+    return doc_data.inserted_id
