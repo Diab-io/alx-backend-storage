e59e6a8a533dd00eca8a0bf63b337890e02a7411
Odimayo David <odimayodavid7@gmail.com>
2023-04-13 22:49:39 +0100

    added file

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 
+    Changes the topics of a document
+    based on the name specified
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ The function that implements updating by name """
+    return mongo_collection.update({"name": name}, {"$set":{"topics": topics}})
